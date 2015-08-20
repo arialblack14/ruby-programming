0eb9432b6e4dfcd91596bcd0f0e7ab38b8056a2b
Alex Tsiras <tsirasalexis@yahoo.gr>
2015-08-20 19:33:53 +0300

    Added bubble_sort_by

--- ruby building blocks/bubble_sort.rb
+++ ruby building blocks/bubble_sort.rb
@@ -1,3 +1,4 @@
+##Bubble Sort using array
 def bubble_sort arr
   n = arr.length - 1
   while n > 0 do 
@@ -12,3 +13,21 @@ def bubble_sort arr
 end
 
 p bubble_sort [4,3,78,2,0,2]
+
+#Bubble Sort using array and block
+def bubble_sort_by arr
+    n = arr.length - 1
+    while n > 0
+        arr.each_with_index do |item, index|
+            yield(arr[index] , arr[index+1]) > 0 ? true : (arr[index+1],                  arr[index] = arr[index], arr[index+1])
+            # To avoid index + 1 being nil when on last iteration (index == n)
+            if index + 1 == n
+                break
+            end
+        end
+        n -= 1
+    end
+    puts arr
+end
+
+bubble_sort_by(["hi","hello","hey", "bye","Mike", "to", "a"]) { |left,right| right.length - left.length }
