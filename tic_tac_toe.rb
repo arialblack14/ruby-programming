799c109450309ac5385031b6e94942a2e1f4c768
arialblack14 <tsirasalexis@yahoo.gr>
2015-08-30 23:55:35 +0300

    Added Mark and Game class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,13 +1,46 @@
 class Player
-	attr_reader = :player_number
+	attr_reader = :first_player, 
+								:second_player
 
-	def initialize player_number
-		@player_number = player_number		
+	def initialize
+		@first_player = first_player
+		@second_player = second_player
 	end
 end
 
-class Choice
-	def initialize chozen
+class Mark
+	attr_reader		:first_player,
+								:second_player
+	attr_accessor :chozen
+
+	def initialize
 		@chozen = chozen		
 	end
+
+	def letter
+		puts "Please choose your mark! X or O?"
+		@chozen = gets.chomp if @chozen == :x || :o	
+	end
 end
+
+class Game
+	# attr_reader :first_player
+	# attr_reader :second_player
+	# attr_reader :chozen
+
+	def initialize
+		@first_player = Player.new
+		@second_player = Player.new
+		@chozen = Mark.new
+	end
+
+	def choose_letter
+		@first_player.letter
+
+	end
+end
+
+
+game = Game.new
+p game.first_player.letter
+
