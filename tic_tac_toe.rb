8e1c13c901d72cf0dfb6274e94ac347727fe39ad
arialblack14 <tsirasalexis@yahoo.gr>
2015-08-31 23:19:58 +0300

    Changed needed classes

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,46 +1,63 @@
 class Player
-	attr_reader = :first_player, 
-								:second_player
+	attr_reader = :player1,
+								:player2,
+								:letter
 
 	def initialize
-		@first_player = first_player
-		@second_player = second_player
+		@player1 = player1
+		@player2 = player2
+	end
+
+	def get_letter
+		puts "Please choose a letter! X or O?"
+		@letter = gets.chomp if error_choice		
+	end
+
+	def error_choice
+		puts "Please choose x or o!" if @letter.downcase
+																					 .include? ("x" || "o")
+																					 || @letter.size > 1
 	end
 end
 
-class Mark
-	attr_reader		:first_player,
-								:second_player
-	attr_accessor :chozen
-
+class Board
 	def initialize
-		@chozen = chozen		
-	end
-
-	def letter
-		puts "Please choose your mark! X or O?"
-		@chozen = gets.chomp if @chozen == :x || :o	
+		
 	end
 end
 
 class Game
-	# attr_reader :first_player
-	# attr_reader :second_player
-	# attr_reader :chozen
+	attr_reader :turn,
+							:game
 
 	def initialize
-		@first_player = Player.new
-		@second_player = Player.new
-		@chozen = Mark.new
+		@board = Board.new
+		@game = game
+		@turn = 0
 	end
 
-	def choose_letter
-		@first_player.letter
+	def start_game
+		puts "************************************"
+		puts "** Welcome to the TicTacToe game! **"
+		puts "************************************"
+		turn_change
+	end
 
+	def turn_change
+		@turn%2 == 0 ? @player1.play : @player2.play
+		@turn++
+	end
+
+	def play
+		
+	end
+
+	def end_game
+		
 	end
 end
 
 
 game = Game.new
-p game.first_player.letter
+p game.player1.choose_letter
 
