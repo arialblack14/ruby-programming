24d814288b6d16d12003c35693715424edbaf846
arialblack14 <tsirasalexis@yahoo.gr>
2015-09-01 10:44:10 +0300

    Added turn changing

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,52 +1,19 @@
 class Player
 	attr_reader = :id,
-								:player
+							  :player
 
 	def initialize id
 		@id = id
-		puts "What is your name player #{@id}?"
+		puts "What is your name player #{@id + 1}?"
 		@player = gets.chomp
-	end
-
-	def play
-		get_letter if @letter.nil?
-		get_position
-	end
-
-	def get_position
-		puts "Please select where to put your mark."
-		puts "Choose a number from 1 to 9."
-		@position = gets.chomp.to_i 
-		if @position.between?(1,9)
-			@board.arr[@position - 1] = @letter
-			puts @board
-		end
-	end
-
-	def get_letter
-		puts "Please choose a letter! X or O?"
-		@letter = gets.chomp 
-		@letter unless error_choice
-		puts "#{@player} has chosen #{@letter}"
-	end
-
-	def error_choice
-		if @letter.length > 1
-			puts "Only one letter please."
-			get_letter
-		end
-
-		unless @letter.downcase == "x" || 
-					 @letter.downcase == "o"
-			puts "ERROR!"
-			get_letter
-		end
 	end
 end
 
 class Board
-	attr_accessor :board,
-								:arr
+	attr_reader 	:turn
+	attr_accessor :arr,
+								:board,
+								:mark
 
 	def initialize
 		@arr = %w{1 2 3 4 5 6 7 8 9}
@@ -55,10 +22,48 @@ class Board
 						 @arr[3] + "|" + @arr[4] + "|" + @arr[5] + "\n" +
 						"-+-+-\n" +
 						 @arr[6] + "|" + @arr[7] + "|" + @arr[8] + "\n"
+		@mark = ["x","o"]
+		@turn = 0
 	end
 
 	def draw
 		puts @board
+		play
+	end
+
+	def play
+		get_position
+	end
+
+	def get_position
+		puts "Please select where to put your letter."
+		puts "Choose a number from 1 to 9."
+		@position = gets.chomp.to_i
+		put_mark
+		puts @board
+		turn_change
+	end
+
+	def what_mark
+		return @turn % 2 == 0 ? @mark[0] : @mark[1] # Select x or o depending on player
+		@turn += 1
+	end
+
+	def put_mark
+		@arr[@position - 1] = what_mark unless error_choice
+	end
+
+	def turn_change
+		9.times do
+			play
+		end
+	end
+
+	def error_choice
+		unless @position.between?(1,9)
+			puts "ERROR!"
+			puts "A number from 1 to 9 please."
+		end
 	end
 end
 
@@ -66,28 +71,25 @@ class Game
 	attr_reader :turn,
 							:game,
 							:player1,
-							:player2
+							:player2,
+							:mark0,
+							:mark1
 
 	def initialize
 		puts "************************************"
 		puts "** Welcome to the TicTacToe game! **"
 		puts "************************************"
 		puts
-		@player1 = Player.new(1)
-		@player2 = Player.new(2)
+		@player1 = Player.new(0)
+		@mark0 = "x"
+		@player2 = Player.new(1)
+		@mark1 = "o"
 		@board = Board.new
-		@turn = 0
 	end
 
 	def start
 		puts "Let the game begin!"
 		@board.draw
-		turn_change
-	end
-
-	def turn_change
-		@turn % 2 == 0 ? @player1.play : @player2.play
-		@turn +=1
 	end
 
 	def end
