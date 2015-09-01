08255c5276ff8166a79263d04e18163a0366035c
arialblack14 <tsirasalexis@yahoo.gr>
2015-09-01 16:38:57 +0300

    Added ability to watch player marks

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -10,6 +10,7 @@ class Player
 end
 
 class Board
+	$winning_combo = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1, 4, 7], [2, 4, 6]]
 	attr_accessor :arr,
 								:board,
 								:turn,
@@ -18,14 +19,18 @@ class Board
 
 	def initialize
 		@arr = %w{1 2 3 4 5 6 7 8 9}
+		@board = board
+		@mark = mark
+		@turn = 0
+		@choices = []
+	end
+
+	def set_board arr
 		@board = @arr[0] + "|" + @arr[1] + "|" + @arr[2] + "\n" +
 						"-+-+-\n" +
 						 @arr[3] + "|" + @arr[4] + "|" + @arr[5] + "\n" +
 						"-+-+-\n" +
 						 @arr[6] + "|" + @arr[7] + "|" + @arr[8] + "\n"
-		@mark = mark
-		@turn = 0
-		@choices = []
 	end
 
 	def draw
@@ -43,7 +48,7 @@ class Board
 		puts "Choose a number from 1 to 9."
 		@position = gets.chomp.to_i
 		error_choice ? @position : get_position
-		puts "@position : #{@position}"
+		# puts "@position : #{@position}"
 		put_mark
 		turn_change
 	end
@@ -53,13 +58,22 @@ class Board
 		return @turn % 2 == 0 ? @mark = "x" : @mark = "o"
 	end
 
-	# Sets array value to the current mark ("x" or "o") and stores players' choices
-	def put_mark
+	# Stores players' choices in individual arrays
+	def player_choices
 		@choices[@turn] = @position - 1
-		puts "choices : #{@choices}"
-		puts "turn : #{@turn}"
+		puts @choices.sort
+		puts "winning_combo : #{$winning_combo}"
+		puts "#{@player} wins!" if $winning_combo.include? @choices.sort
+		puts "choices : #{@choices.sort}"
+	end
+
+	# Sets array value to the current mark ("x" or "o")
+	def put_mark
+		player_choices		
+		# puts "turn : #{@turn}"
 		@arr[@position - 1] = what_mark
 		puts "arr : #{@arr}"
+		set_board @arr
 		puts @board
 		@turn += 1
 	end
