250b23488762f74f07f4b424b2536ed40f068298
arialblack14 <tsirasalexis@yahoo.gr>
2015-09-13 23:15:22 +0300

    Added CodeBraker class

--- project-OOP/mastermind.rb
+++ project-OOP/mastermind.rb
@@ -7,12 +7,12 @@ class Game
 
 	def start
 		puts "Welcome to MASTERMIND!"
-		puts "Would you like to (p)lay or (q)uit?"
+		puts "Would you like to (P)lay or (Q)uit?"
 		choose
 	end
 
 	def choose
-		ans = gets.chomp
+		ans = gets.chomp.downcase
 		ans == "p" ? play : quit
 	end
 
@@ -43,20 +43,22 @@ class Board
 	attr_reader :guess, :count_guess
 
 	def initialize
-		@guess = 1111
+		@guess = "rrrr"
 		@count_guess = 0
-		@number = Random.rand(1111..6666)
+		# Picks 1 each time from the array and joins in the end
+		@secret = 4.times.map { ['r', 'g', 'b', 'y'].sample }.join
+		#puts @secret
 	end
 
 	def start
 		puts "What is your guess...?"
-		@guess = gets.chomp.to_i
+		@guess = gets.chomp
 		puts "Your guess is #{@guess}."
 		check_guess @guess
 	end
 
 	def check_guess guess
-		@guess == @number ? (puts "win") : (puts "lose")
+		@guess == @secret ? (puts "win") : (puts "lose")
 		@count_guess += 1
 		puts "You have " + (12 - @count_guess).to_s + " guesses remaining."
 		reguess
@@ -67,7 +69,10 @@ class Board
 	end
 end
 
-p = Player.new("Alex")
+class CodeBreaker < Player
+end
+
+p = CodeBreaker.new("Alex")
 p.hello
 g = Game.new
 g.start
